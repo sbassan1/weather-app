3605ef64849c7de5ceaf8e61f88bcb99ba5d2521
Santiago Bassani <santiagobassani2000@gmail.com>
2025-02-17 19:15:54 -0300

    Added functional time, just for BsAs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./styles/styles.css"
 
+const months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+];
 
+const days = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+];
+
+// Update this to get the day/month/years updated
+setInterval(function(){
+    let date = new Date();
+
+    let second = date.getSeconds();
+    let minute = date.getMinutes();
+    let hour = date.getHours();
+    let dayName = date.getDay();
+    let dayNum = date.getDate();
+    let monthNum = date.getMonth();
+    let year = date.getFullYear();
+
+    document.getElementById('date').innerHTML = days[dayName] + " ," + dayNum + ", " + months[monthNum] + ", " + year;
+    document.getElementById('time').innerHTML = hour + ":" + minute + ":" + second;
+    },1000);
